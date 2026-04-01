d2f70a48a6e5eeb2b154bca16ed25a765c2287b9
So Bunleng <so.bunleng246800@gmail.com>
2026-04-02 00:14:24 +0700

    fix bug link url

--- lib/api.ts
+++ lib/api.ts
@@ -6,6 +6,7 @@ export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080/api/v1",
   headers: {
     "Content-Type": "application/json",
+    "ngrok-skip-browser-warning": "true",
   },
   timeout: 10000,
 });
